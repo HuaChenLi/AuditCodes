095309f7eb21d75149d576219fbce3964da6b32d
Hua Chen Li <hua-chenli@hotmail.com>
2023-08-03 21:59:40 +1000

    Random comment cause pushing is hard

--- Business Audit/move_deets_from_bank_csv_to_excel_sheets.py
+++ Business Audit/move_deets_from_bank_csv_to_excel_sheets.py
@@ -5,6 +5,7 @@ from lxml import etree
 from lib_jank_folder.lib_jank import *
 
 # I should add a ReadMe for me so I know what to do
+# Another comment
 
 # Set the quarter and financial year
 quarter = 4
