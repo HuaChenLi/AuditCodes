5421e0cde67463c96dd8734783e8db84c7f576ef
= <=>
2023-08-05 10:58:12 +1000

    Made the SQL functions work with all the connection details and stuff in the SQL python file. This means if for some reason I migrate the database, or the password gets changed, I would only need to change it in 1 place.

--- SQLFunctions/select_mappings.py
+++ SQLFunctions/select_mappings.py
@@ -1,7 +1,19 @@
-def select_mapping_query(auditID, incomeExpenseChar) : 
-    
-    return f"""
+import pandas as pd
+
+from SQLFunctions.sql_connection_functions import *
+
+password = "Bdvej746Js$2jd"
+database = "MyDataBase"
+
+connection = create_db_connection("localhost", "root", password, database)
+
+
+def select_mapping_query(auditID, incomeExpenseChar):
+    query = f"""
     SELECT id,map_from,map_to FROM mapping_table 
     INNER JOIN mapping_selection ON AuditID = MappingTableID 
     WHERE AuditID = {auditID} AND (IncomeExpense = '{incomeExpenseChar}' OR IncomeExpense = 'B')
     """
+
+    return pd.DataFrame.from_records(read_query(connection, query), columns=["id", "map_from", "map_to"])
+
