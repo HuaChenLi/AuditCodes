ecdc2bc7b53137a30505ab892c0cd2fe18c62273
= <=>
2023-08-05 10:20:16 +1000

    Successfully changed Nested List into Dataframe type. Now need to consider how I want the SQL query functions to work

--- SQLFunctions/select_mappings.py
+++ SQLFunctions/select_mappings.py
@@ -1,7 +1,7 @@
 def select_mapping_query(auditID, incomeExpenseChar) : 
     
     return f"""
-    SELECT map_from,map_to FROM mapping_table 
+    SELECT id,map_from,map_to FROM mapping_table 
     INNER JOIN mapping_selection ON AuditID = MappingTableID 
     WHERE AuditID = {auditID} AND (IncomeExpense = '{incomeExpenseChar}' OR IncomeExpense = 'B')
     """
---
+++ TestSQLScripts/convert_regex_mappings_to_query.py
@@ -0,0 +1,63 @@
+from sql_connection_functions import *
+
+
+
+password = "Bdvej746Js$2jd"
+database = "MyDataBase"
+
+connection = create_server_connection("localhost", "root", password)
+connection = create_db_connection("localhost", "root", password, database)
+
+business_list = [
+        ["Hero Sushi", " Hero Sushi"]
+        ["Top Notch", " Top Notch"]
+        ["EG GROUP", " EG Group"]
+        ["BELONG", " Belong"]
+        ["Belong", " Belong"]
+        ["MCDONALDS", " McDonalds"]
+        ["GOLD LEAF", " Gold Leaf"]
+        ["CRUNCHYROLL", " Crunchyroll"]
+        ["UBER", " Uber"]
+        ["SPOTIFY", " Spotify"]
+        ["NoodleBox", " Noodlebox"]
+        ["Mighty Moonee Ponds", " Mighty Moonee Ponds"]
+        ["Zeus Street Greek", " Zeus Street Greek"]
+        ["SUPA IGA", " IGA"]
+        ["COLES EXPRESS", " Coles Express"]
+        ["OPTUS", " Optus"]
+        ["EASTLINK", " Eastlink"]
+        ["ENERGYAUSTRALIA", " EnergyAustralia"]
+        ["TANGO", " Tango Energy"]
+        ["JB HI-FI", " JB Hi-Fi"]
+        ["WOOLWORTHS", " Woolworths"]
+        ["MYKI", " Myki"]
+        ["Sir Duke", " Sir Duke"]
+        ["7-ELEVEN", " 7-Eleven"]
+        ["HUNKY DORY", " Hunky Dory"]
+        ["KMART", " Kmart"]
+        ["CHEM WAREHS", " Chemist Warehouse"]
+        ["CHEMIST WAREHOUSE", " Chemist Warehouse"]
+        ["HEALTHY PETS", " Healthy Pets"]
+        ["ALLISON BROWNING", " Allison Browning"]
+        ["COMMONWEALTH INSURANCE", " Commonwealth Insurance"]
+        ["BUNNINGS", " Bunnings"]
+        ["Yarra Valley Water", " Yarra Valley Water"]
+        ["KFC", " KFC"]
+        ["XERO", " Xero"]
+        ["DR BILGE", " Dr Bilge"]
+        ["SPOTLIGHT", " Spotlight"]
+        ["ALLIANZ", " Allianz"]
+        ["MasterCardPayment", " Mastercard Payment"]
+        ["RACV", " RACV"]
+        ["ALDI", " ALDI"]
+        ["Hungry Jacks", " Hungry Jacks"]
+        ["Home Loan", " Home Loan"]
+        ["Loan Repayment", " Home Loan"]
+        ["AIA AUSTRALIA", " AIA Australia"]
+        ["Fast Transfer From Everkeen", " Everkeen"]
+]
+
+
+
+# I need a flow chart
+# Excalidraw
