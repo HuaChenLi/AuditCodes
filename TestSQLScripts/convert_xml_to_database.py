425d0326bcba35151440ef37394e4beadc781eb4
= <=>
2023-08-07 22:11:45 +1000

    Successfully moved all the XML data for my bank account into the database. Now I need to change the program to actually use the data. After that, then I would need to do the same for the Business Accounts. So I need to duplicate the tables and test on the temp tables.

--- TestSQLScripts/convert_xml_to_database.py
+++ TestSQLScripts/convert_xml_to_database.py
@@ -4,34 +4,43 @@ from lxml import etree
 root = etree.parse("C:\\Users\hua-c\Desktop\Coding Stuff\Python Coding\Column Rules\my_account_rules.xml")
 
 income_col_names = root.findall(".//IncomeColumns//Column")
+expense_col_names = root.findall(".//ExpenseColumns//Column")
 
 column_name_list = []
 
-for index, col_name in enumerate(income_col_names, 1):
-    column_name = col_name.find(".//ColumnName").text
-    column_name_list.append(column_name)
+for direction in ["I", "E"]:
+    if direction == "I":
+        temp_column_names = income_col_names
+        isIncome = True
+    else:
+        temp_column_names = expense_col_names
+        isIncome = False
 
-    SQLFunctions.excel_columns.insert_excel_column(1, column_name, False, True, True)
+    for index, col_name in enumerate(temp_column_names, 1):
+        column_name = col_name.find(".//ColumnName").text
+        column_name_list.append(column_name)
 
-    temp_values_list = list(col_name.iter("Value"))
+        SQLFunctions.excel_columns.insert_excel_column(1, column_name, False, True, isIncome)
 
-    last_column_id = SQLFunctions.excel_columns.select_last_column_id()
+        temp_values_list = list(col_name.iter("Value"))
 
-    for value in temp_values_list:
-        value_exists_in_mapping = False
+        last_column_id = SQLFunctions.excel_columns.select_last_column_id()
 
-        CategoryMappingValues = SQLFunctions.excel_columns.select_all_excel_category_mapping()
+        for value in temp_values_list:
+            value_exists_in_mapping = False
 
-        existing_category_mapping_id = 0
+            CategoryMappingValues = SQLFunctions.excel_columns.select_all_excel_category_mapping()
 
-        for i, r in CategoryMappingValues.iterrows():
-            if r["CategoryValues"] == value.text:
-                existing_category_mapping_id = r["ExcelCategoryMappingID"]
-                value_exists_in_mapping = True
+            existing_category_mapping_id = 0
 
-        if value_exists_in_mapping:
-            temp_table = SQLFunctions.excel_columns.select_excel_category_mapping_id(existing_category_mapping_id)
-            SQLFunctions.excel_columns.insert_excel_columns_selection(last_column_id, existing_category_mapping_id)
-            print("here")
-        else:
-            SQLFunctions.excel_columns.insert_excel_category_mapping(last_column_id, value.text)
+            for i, r in CategoryMappingValues.iterrows():
+                if r["CategoryValues"] == value.text:
+                    existing_category_mapping_id = r["ExcelCategoryMappingID"]
+                    value_exists_in_mapping = True
+
+            if value_exists_in_mapping:
+                temp_table = SQLFunctions.excel_columns.select_excel_category_mapping_id(existing_category_mapping_id)
+                SQLFunctions.excel_columns.insert_excel_columns_selection(last_column_id, existing_category_mapping_id)
+                print("here")
+            else:
+                SQLFunctions.excel_columns.insert_excel_category_mapping(last_column_id, value.text)
