6cf48e21483e5b10ca591d8cadfecf11cfbafc01
HuaChenLi <huachenli30@gmail.com>
2023-08-08 22:16:03 +1000

    XML references ripped out from move_my_bank_deets_csv.py. Wooohoooo!!!!! Now need to perform the similar SQL rule to convert the Business XMLs into the database.

--- CommonLibrary/getting_replacement_values.py
+++ CommonLibrary/getting_replacement_values.py
@@ -1,7 +1,7 @@
 from SQLFunctions.select_mappings import select_mapping_query
 
 
-def replacement_values(audit_id, incomeExpenseChar):
+def replacement_values(audit_id, income_expense_char):
     replacements_values = {
         "Description": {
             r"\b[0-9]+\b\/\b[0-9]+\b\/\b[0-9]+\b": "",  # dates
@@ -15,7 +15,7 @@ def replacement_values(audit_id, incomeExpenseChar):
             r"^\s": ""}  # white space at start of description
     }
 
-    data_sql_1 = select_mapping_query(audit_id, incomeExpenseChar)
+    data_sql_1 = select_mapping_query(audit_id, income_expense_char)
 
     for i, r in data_sql_1.iterrows():
         mappedFromValue = r["map_from"]
