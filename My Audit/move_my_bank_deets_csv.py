19760d2bfb894c9446c1a4df35c7d208e8d5b3e6
HuaChenLi <huachenli30@gmail.com>
2023-08-10 21:09:57 +1000

    Finished making a chunk a code into another python library

--- My Audit/move_my_bank_deets_csv.py
+++ My Audit/move_my_bank_deets_csv.py
@@ -4,10 +4,9 @@ import shutil
 import sys
 import time
 
-import SQLFunctions.sql_excel_columns
 from SQLFunctions.select_mappings import *
 from CommonLibrary.date_libraries import month_period
-from CommonLibrary.getting_replacement_values import replacement_values
+import CommonLibrary.create_build_income_expense_data
 
 sys.path.append(os.path.abspath("CommonLibrary"))
 
@@ -67,60 +66,11 @@ csv_data.sort_values(by="Date", inplace=True)
 for sheet_title in ["Income", "Expenditure"]:
     if sheet_title == "Income":
         is_income = True
-        incomeExpenseChar = "I"
     else:
         is_income = False
-        incomeExpenseChar = "E"
 
-    replacement_value = replacement_values(auditID, incomeExpenseChar)
-    associated_values_dictionary = dict()
-
-    column_dataframe = SQLFunctions.sql_excel_columns.select_excel_column(audit_id=auditID, is_income=is_income)
-    column_name_list = ["Date", "Description"] + list(column_dataframe["ColumnName"])
-
-    for index, row in column_dataframe.iterrows():
-        column_name = row["ColumnName"]
-        excel_column_id = row["ExcelColumnID"]
-        temp_df = SQLFunctions.sql_excel_columns.select_excel_category_mapping_values(excel_column_id)
-
-        values_list = list(temp_df["CategoryValues"])
-
-        associated_values_dictionary.setdefault(column_name, values_list)
-
-    excel_sheet = pd.DataFrame(columns=column_name_list)
-
-    for index, row in csv_data.iterrows():
-
-        # creating the Income data frame 
-        if sheet_title == "Income" and csv_data.at[index, "Amount"] > 0 or sheet_title != "Income" and csv_data.at[index, "Amount"] < 0:
-
-            excel_sheet.at[index, "Date"] = csv_data.at[index, "Date"]
-            excel_sheet.at[index, "Description"] = csv_data.at[index, "Description"]
-
-            try:
-                default_column_df = SQLFunctions.sql_excel_columns.select_default_excel_column(auditID, is_income)
-                default_column_id = default_column_df[0][0]
-                default_column_name = default_column_df[0][1]
-            except Exception:
-                default_column_name = column_name_list[2]
-            # convert negative to positive for expense
-            if sheet_title != "Income" and csv_data.at[index, "Amount"] < 0:
-                csv_data.at[index, "Amount"] = csv_data.at[index, "Amount"] * (-1)
-
-            is_categorised = False
-            for category_name, category_values in associated_values_dictionary.items():
-                if any(value.casefold() in row[2].casefold() for value in category_values):
-                    excel_sheet.at[index, category_name] = csv_data.at[index, "Amount"]
-                    is_categorised = True
-                    break
-
-            if not is_categorised:
-                excel_sheet.at[index, default_column_name] = csv_data.at[index, "Amount"]
-
-    excel_sheet = excel_sheet.replace(replacement_value, regex=True)
-
+    excel_sheet = CommonLibrary.create_build_income_expense_data.build(auditID, is_income, csv_data)
     output_filepath = os.path.join(root_excel_directory, sheet_title + ".csv")
-
     print(output_filepath)
     excel_sheet.to_csv(output_filepath, index=False)
 
