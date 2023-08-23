a59adbff2e79d8c4dfdedb39b8b0e2013cf7cf7a
HuaChenLi <huachenli30@gmail.com>
2023-08-23 21:24:08 +1000

    Finished the really rough layout for the program. Now need to choose which button to do first

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ import java.io.*;
 
 public class Main {
     JFrame frame;
-    JPanel actionPanel, titlePanel, mainPanel, mappingPanel;
-    JLabel titleLabel, mappingFromLabel, mappingToLabel, blankLabel;
-    JButton createExcelSheets, createIncomeExpenseCSVs, createMapping;
-    JTextField financialYear, mappingFrom, mappingTo;
+    JPanel actionPanel, titlePanel, mainPanel, mappingPanel, financialYearPanel, incomeExpensePanel, accountSelectionPanel;
+    JLabel financialYearLabel, financialYearIndicator, titleLabel, mappingFromLabel, mappingToLabel, blankLabel, incomeExpenseLabel, incomeExpenseIndicator, accountSelectionLabel;
+    JButton createExcelSheets, createIncomeExpenseCSVs, createMapping, setFinancialYear, incomeButton, expenseButton, accountSelectionButton;
+    JTextField financialYearText, mappingFrom, mappingTo, accountSelectionText;
 
 
 
@@ -17,60 +17,49 @@ public class Main {
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
 
-
-
+//        Title Panel
         titlePanel = new JPanel();
         titleLabel = new JLabel("One Stop Shop for Excel Shenanigans");
         titlePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         titlePanel.add(titleLabel);
 
+        mainPanel.add(titlePanel);
 
+//        Financial Year Panel
+        financialYearLabel = new JLabel("Financial Year");
+        financialYearIndicator = new JLabel();
+        financialYearText = new JTextField();
+        setFinancialYear = new JButton("Set Financial Year");
+        financialYearPanel = new JPanel();
+        financialYearPanel.setLayout(new GridLayout(0,4));
+        financialYearPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        financialYearPanel.add(financialYearLabel);
+        financialYearPanel.add(financialYearIndicator);
+        financialYearPanel.add(financialYearText);
+        financialYearPanel.add(setFinancialYear);
 
+        mainPanel.add(financialYearPanel);
 
+//        Account Selection
+        accountSelectionPanel = new JPanel();
+        accountSelectionPanel.setLayout(new GridLayout(0,3));
+        accountSelectionPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        accountSelectionLabel = new JLabel();
+        accountSelectionText = new JTextField();
+        accountSelectionButton = new JButton("Set Account");
+        accountSelectionPanel.add(accountSelectionLabel);
+        accountSelectionPanel.add(accountSelectionText);
+        accountSelectionPanel.add(accountSelectionButton);
+
+        mainPanel.add(accountSelectionPanel);
+
+//        Action Panel
         actionPanel = new JPanel();
         actionPanel.setLayout(new GridLayout(0,2));
-        actionPanel.setBorder(BorderFactory.createEmptyBorder(10,10,100,10));
-
-        frame = new JFrame();
+        actionPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
         createExcelSheets = new JButton("Create New Excel Sheets");
         createIncomeExpenseCSVs = new JButton("Create Income and Expense CSVs");
-
-        actionPanel.add(createExcelSheets);
-        actionPanel.add(createIncomeExpenseCSVs);
-
-        mainPanel.add(titlePanel);
-        mainPanel.add(actionPanel);
-
-
-//        Mapping Panel
-        mappingPanel = new JPanel();
-        mappingPanel.setLayout(new GridLayout(0,3));
-        mappingPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-
-        mappingFromLabel = new JLabel("Map From");
-        mappingToLabel = new JLabel("Map To");
-        blankLabel = new JLabel();
-
-        createMapping = new JButton("Create Mapping");
-        mappingFrom = new JTextField();
-        mappingTo = new JTextField();
-
-        mappingPanel.add(mappingFromLabel);
-        mappingPanel.add(mappingToLabel);
-        mappingPanel.add(blankLabel);
-        mappingPanel.add(mappingFrom);
-        mappingPanel.add(mappingTo);
-        mappingPanel.add(createMapping);
-
-        mainPanel.add(mappingPanel);
-
-
-        frame.add(mainPanel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Bank Account Organisation");
-        frame.pack();
-        frame.setVisible(true);
 
         createExcelSheets.addActionListener(e1 -> {
             try {
@@ -88,14 +77,68 @@ public class Main {
             }
         });
 
+
+        actionPanel.add(createExcelSheets);
+        actionPanel.add(createIncomeExpenseCSVs);
+
+        mainPanel.add(actionPanel);
+
+//        Income Expense Indicator Panel
+        incomeExpensePanel = new JPanel();
+        incomeExpensePanel.setLayout(new GridLayout(0,4));
+        incomeExpensePanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        incomeExpenseLabel = new JLabel("Income and/or Expense");
+        incomeExpenseIndicator = new JLabel();
+        incomeButton = new JButton("Income");
+        expenseButton = new JButton("Expense");
+        incomeExpensePanel.add(incomeExpenseLabel);
+        incomeExpensePanel.add(incomeExpenseIndicator);
+        incomeExpensePanel.add(incomeButton);
+        incomeExpensePanel.add(expenseButton);
+
+        mainPanel.add(incomeExpensePanel);
+
+//        Mapping Panel
+        mappingPanel = new JPanel();
+        mappingPanel.setLayout(new GridLayout(0,3));
+        mappingPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        mappingFromLabel = new JLabel("Map From");
+        mappingToLabel = new JLabel("Map To");
+        blankLabel = new JLabel();
+
+        createMapping = new JButton("Create Mapping");
         createMapping.addActionListener(e1 -> {
             try{
                 createMappingFunction();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         });
+
+        mappingFrom = new JTextField();
+        mappingTo = new JTextField();
+
+        mappingPanel.add(mappingFromLabel);
+        mappingPanel.add(mappingToLabel);
+        mappingPanel.add(blankLabel);
+        mappingPanel.add(mappingFrom);
+        mappingPanel.add(mappingTo);
+        mappingPanel.add(createMapping);
+
+        mainPanel.add(mappingPanel);
+
+//        Setting the GUI Frame
+        frame = new JFrame();
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Bank Account Organisation");
+        frame.pack();
+        frame.setVisible(true);
+
+
+
 
     }
 
