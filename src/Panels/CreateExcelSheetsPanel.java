fb2a1d67c65665d37a9685ac1e606fddf1bc9860
HuaChenLi <huachenli30@gmail.com>
2025-01-01 22:52:34 +1100

    now can multiselect files

--- src/Panels/CreateExcelSheetsPanel.java
+++ src/Panels/CreateExcelSheetsPanel.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -123,17 +124,21 @@ public class CreateExcelSheetsPanel extends JPanel {
                             JFileChooser chooser = new JFileChooser();
                             FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV Files (*.csv)", "csv");
                             chooser.setFileFilter(filter);
+                            chooser.setMultiSelectionEnabled(true);
 
                             if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-                                String file = chooser.getSelectedFile().getAbsolutePath();
-
-                                if (!isDuplicateFile(file)) {
-                                    csvFiles.add(file);
-                                    csvTableModel.addRow(new Object[]{file});
-                                    csvTable.setModel(csvTableModel);
-                                } else {
-                                    System.out.println("Duplicate File");
+                                File[] files = chooser.getSelectedFiles();
+                                for (File f : files) {
+                                    String file = f.getAbsolutePath();
+                                    if (!isDuplicateFile(file)) {
+                                        csvFiles.add(file);
+                                        csvTableModel.addRow(new Object[]{file});
+                                        csvTable.setModel(csvTableModel);
+                                    } else {
+                                        System.out.println("Duplicate File");
+                                    }
                                 }
+
                             }
                         }
                     });
