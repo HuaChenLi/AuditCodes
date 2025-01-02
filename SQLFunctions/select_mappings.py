0906516cc0bb4f8771917f2ac6fdb3d7bb157391
HuaChenLi <huachenli30@gmail.com>
2025-01-02 22:24:29 +1100

    removed unused functions

--- SQLFunctions/select_mappings.py
+++ SQLFunctions/select_mappings.py
@@ -20,27 +20,3 @@ def select_mapping_query(auditID, incomeExpenseChar):
     """
 
     return pd.DataFrame.from_records(read_query(connection, query), columns=["id", "map_from", "map_to"])
-
-
-def insert_mapping_selection(audit_id, mapping_table_id, income_expense_char):
-    query = f"""
-    INSERT INTO mapping_selection (audit_id, mapping_table_id, income_expense)
-    VALUES ({audit_id}, {mapping_table_id}, '{income_expense_char}')    
-    """
-    execute_query(connection, query)
-
-
-def insert_mapping_table_and_mapping_selection(map_from, map_to, audit_id, income_expense_char):
-    query = f"""
-    INSERT INTO mapping_table (map_from, map_to)
-    VALUES ('{map_from}', '{map_to}')
-    """
-    execute_query(connection, query)
-
-    last_id_value_query = f"""
-    SELECT MAX(id) FROM mapping_table 
-    """
-    last_id = read_query(connection, last_id_value_query)
-    insert_mapping_selection(audit_id, last_id[0][0], income_expense_char)
-
-
