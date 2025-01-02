af0facffc41009db9e683965c699276fa1bdd983
HuaChenLi <huachenli30@gmail.com>
2025-01-02 22:16:24 +1100

    moved default column logic so it's only run once and stopped the python from crashing

--- CommonLibrary/build_income_expense_data.py
+++ CommonLibrary/build_income_expense_data.py
@@ -15,6 +15,12 @@ def build(audit_id, is_income, data_frame):
     column_dataframe = SQLFunctions.sql_excel_columns.select_excel_column(audit_id=audit_id, is_income=is_income)
     column_name_list = ["Date", "Description"] + list(column_dataframe["column_name"])
 
+    try:
+        default_column_df = SQLFunctions.sql_excel_columns.select_default_excel_column(audit_id, is_income)
+        default_column_name = default_column_df[0][1]
+    except Exception:
+        default_column_name = column_name_list[2]
+
     for index, row in column_dataframe.iterrows():
         column_name = row["column_name"]
         excel_column_id = row["id"]
@@ -24,26 +30,22 @@ def build(audit_id, is_income, data_frame):
 
         associated_values_dictionary.setdefault(column_name, values_list)
 
+    columnAndCategory = associated_values_dictionary.items()
+
     excel_sheet = pd.DataFrame(columns=column_name_list)
 
     for index, row in csv_data.iterrows():
-        # creating the Income and Expense data frame
         if is_income and csv_data.at[index, "Amount"] > 0 or not is_income and csv_data.at[index, "Amount"] < 0:
             excel_sheet.at[index, "Date"] = csv_data.at[index, "Date"]
             excel_sheet.at[index, "Description"] = csv_data.at[index, "Description"]
 
-            try:
-                default_column_df = SQLFunctions.sql_excel_columns.select_default_excel_column(audit_id, is_income)
-                default_column_name = default_column_df[0][1]
-            except Exception:
-                default_column_name = column_name_list[2]
             # convert negative to positive for expense
             if not is_income and csv_data.at[index, "Amount"] < 0:
                 csv_data.at[index, "Amount"] = csv_data.at[index, "Amount"] * (-1)
 
             is_categorised = False
-            for category_name, category_values in associated_values_dictionary.items():
-                if any(value.casefold() in row[2].casefold() for value in category_values):
+            for category_name, category_values in columnAndCategory:
+                if any(value.casefold() in row["Description"].casefold() for value in category_values):
                     excel_sheet.at[index, category_name] = csv_data.at[index, "Amount"]
                     is_categorised = True
                     break
@@ -55,5 +57,3 @@ def build(audit_id, is_income, data_frame):
     excel_sheet = excel_sheet.replace(rv, regex=True)
 
     return excel_sheet
-
-
