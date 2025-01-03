2f010eeb9877ed01af3a8c4c3a39e481925a705f
HuaChenLi <huachenli30@gmail.com>
2025-01-03 23:46:51 +1100

    added error box

--- src/Panels/AlertMessage.java
+++ src/Panels/AlertMessage.java
@@ -5,6 +5,11 @@ import javax.swing.*;
 public class AlertMessage {
     public static void infoBox(String infoMessage, String titleBar)
     {
-        JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void errorBox(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.ERROR_MESSAGE);
     }
 }
--- src/Panels/CreateExcelSheetsPanel.java
+++ src/Panels/CreateExcelSheetsPanel.java
@@ -6,8 +6,6 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -94,11 +92,17 @@ public class CreateExcelSheetsPanel extends JPanel {
             System.out.println("lines " + lines);
         }
 
+        boolean isErrorLineExists = false;
         while ((lines=readers.readLine())!=null) {
             System.out.println("Error lines " + lines);
+            isErrorLineExists = true;
         }
 
-        AlertMessage.infoBox("Excel Sheets Created", "Finished Process");
+        if (isErrorLineExists) {
+            AlertMessage.errorBox("Excel Sheets Not Created", "Finished Process");
+        } else {
+            AlertMessage.infoBox("Excel Sheets Created", "Finished Process");
+        }
     }
 
 
