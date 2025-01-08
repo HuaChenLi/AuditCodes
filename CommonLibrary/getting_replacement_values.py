fba971fbbcc24aece01f81aab689f1e275c10599
HuaChenLi <huachenli30@gmail.com>
2025-01-08 21:42:22 +1100

    removed some predetermined mapping as it was confusing

--- CommonLibrary/getting_replacement_values.py
+++ CommonLibrary/getting_replacement_values.py
@@ -4,10 +4,10 @@ from SQLFunctions.select_mappings import select_mapping_query
 def replacement_values(audit_id, income_expense_char):
     rep_values = {
         "Description": {
-            r"Value Date:\s\b[0-9]+\b\/\b[0-9]+\b\/\b[0-9]+\b$": "",  # dates
-            r"\*": "",  # remove asterisks
-            r"(\s){2,}": "",  # multiple white spaces
-            r"^\s": ""}  # white space at start of description
+            # r"Value Date:\s\b[0-9]+\b\/\b[0-9]+\b\/\b[0-9]+\b$": "",  # dates
+            # r"(\s){2,}": "",  # multiple white spaces
+            # r"^\s": "" # white space at start of description
+            }
     }
 
     data_sql_1 = select_mapping_query(audit_id, income_expense_char)
